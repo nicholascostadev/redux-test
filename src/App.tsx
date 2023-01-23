06e53ac61008e274f8b7bd9bfeaaa62092a4e603
Nicholas Costa <nicholascostadev@gmail.com>
2023-01-23 19:25:19 -0300

    feat: add dogs api with RTK

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { FormEvent, useState } from 'react';
 import { useAppDispatch, useAppSelector } from './app/hooks';
 import { amountAdded, incremented } from './features/counter/counterSlice';
+import { dogsApiSlice } from './features/dogs/dogsApiSlice';
+import { useFetchBreedsQuery } from './features/dogs/dogsApiSlice';
 
 function App() {
   const [value, setValue] = useState(0);
+  const [numDogs, setNumDogs] = useState(10);
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
 
   const handleClick = () => {
     dispatch(incremented());
@@ -19,7 +24,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="flex flex-col gap-4 justify-center items-center h-screen">
+      <div className="flex flex-col gap-4 justify-center items-center min-h-screen">
         <div className="flex gap-2 justify-center items-center">
           <div>
             <button
@@ -41,6 +46,50 @@ function App() {
                 onChange={(e) => setValue(Number(e.target.value))}
               />
             </form>
+          </div>
+          <span>Current Count: {count}</span>
+        </div>
+        <div className="flex gap-2 justify-center items-center">
+          <div>
+            <div className="flex flex-col gap-2 justify-center items-center">
+              <p>Number of dogs fetched: {data.length}</p>
+              <div className="flex items-center justify-center gap-4">
+                <p>Dogs to fetch</p>
+                <select
+                  value={numDogs}
+                  onChange={(e) => setNumDogs(Number(e.target.value))}
+                  className="bg-gray-800 rounded-md p-1"
+                >
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+                </select>
+              </div>
+            </div>
+
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Picture</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((breed) => (
+                  <tr key={breed.id}>
+                    <td>{breed.name}</td>
+                    <td>
+                      <img
+                        src={breed.image.url}
+                        alt={breed.name}
+                        height={250}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
           <span>Current Count: {count}</span>
         </div>
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { dogsApiSlice } from '../features/dogs/dogsApiSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [dogsApiSlice.reducerPath]: dogsApiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(dogsApiSlice.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
